9d07c1dc2cdea12086da050517fbc46788c6b4e8
Perla20 <perlablancoq@hotmail.com>
2017-10-09 18:30:52 -0500

    añadir solucion

---
+++ Cifrado César/app.js
@@ -0,0 +1,29 @@
+var word =prompt("¿Cuál es tu palabra?")
+//var word= function(str);
+//var str.slice();
+//console.log(word(str));
+
+//var word =prompt("¿Cuál es tu palabra?")
+//var word= function(str){
+
+  /*for(var i=0;i<str.length;i++){
+    if(slice(0,1)===a){
+    return 65;
+     }
+      if(slice(0,1)===b){
+     return 66;
+     }
+      if(slice(0,1)===c){
+      }return 67;
+
+
+}
+}
+console.log(word("hilo"));*/
+//var word= function(str){
+
+  //for(var i=0;i<str.length;i++){
+  //  return(slice()-65+33)%26+65;
+  //}
+//}
+  //console.log(str);
