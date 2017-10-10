7e8caf82fb4b2cf4bcc6e3cbb5ca386cf1d8b208
Perla20 <31582436+Perla20@users.noreply.github.com>
2017-10-10 03:07:49 -0500

    Update app.js

--- Cifrado César/app.js
+++ Cifrado César/app.js
@@ -1,7 +1,14 @@
- var word=(prompt("Escribe una palabra"));
- 
-console.log(word.charCodeAt());
 
-}
+ var word=(prompt("Escribe una palabra"));
+ var code=(word.charCodeAt());
+console.log(code);
+
+  
+  var cipher=(code-33)% 26;
+  console.log( cipher);
+ /* function decipher=word.fromCharCode(cipher){
+    return decipher;
+  }
+  console.log(decipher);*/
 
 
